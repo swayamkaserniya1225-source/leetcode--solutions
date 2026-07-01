8459aede94d922a089e1192cfa72afcb1e23e54e
swayamkaserniya1225-source <swayamkaserniya1225@gmail.com>
2026-07-01 12:54:37 +0530

    Time: 8 ms (87.90%) | Memory: 19.3 MB (68.18%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -7,7 +7,7 @@ class Solution:
             while s[i] in s1:
                 s1.remove(s[first])
                 first+=1
-            max2=max(max2,len(s1)+1) 
+            max2=max(max2,i-first+1) 
             s1.add(s[i])
         return max2
             
