c4968b8cf7ad1573e9600751997ec8af06dc31e2
swayamkaserniya1225-source <swayamkaserniya1225@gmail.com>
2026-07-02 14:03:30 +0530

    Time: 1 ms (53.01%) | Memory: 19.4 MB (15.44%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.py
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -1,5 +1,31 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        def merge_sort(list1):
+            if len(list1)>1:
+                mid=len(list1)//2
+                left=list1[:mid]
+                right=list1[mid:]
+                merge_sort(left)
+                merge_sort(right)
+                i=j=k=0
+                while i<len(left) and j<len(right):
+                    if left[i]<right[j]:
+                        list1[k]=left[i]
+                        i+=1
+                    else:
+                        list1[k]=right[j]
+                        j+=1
+                    k+=1
+                while i<len(left):
+                    list1[k]=left[i]
+                    i+=1
+                    k+=1
+                while j<len(right):
+                    list1[k]=right[j]
+                    j+=1
+                    k+=1
+            return list1
+
         count1={}
         for num in arr1:
             count1[num]=count1.get(num,0)+1
@@ -15,5 +41,5 @@ class Solution:
             for _ in range(value):
                 arr1[i]=key
                 i+=1
-        arr1[k:]=sorted(arr1[k:])
+        arr1[k:]=merge_sort(arr1[k:])
         return arr1
