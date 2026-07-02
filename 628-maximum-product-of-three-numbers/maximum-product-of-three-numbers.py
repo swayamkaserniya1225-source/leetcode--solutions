5819571eeda8efe842ee32cb2fb89743b7f7fe4f
swayamkaserniya1225-source <swayamkaserniya1225@gmail.com>
2026-07-02 14:58:32 +0530

    Time: 14 ms (78.91%) | Memory: 20.3 MB (78.20%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.py
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.py
@@ -1,28 +1,21 @@
 class Solution:
     def maximumProduct(self, nums: List[int]) -> int:
-        def merge_sort(list1):
-            if len(list1)>1:
-                mid=len(list1)//2
-                left=list1[:mid]
-                right=list1[mid:]
-                merge_sort(left)
-                merge_sort(right)
-                i=j=k=0
-                while i<len(left) and j<len(right):
-                    if left[i]<right[j]:
-                        list1[k]=left[i]
-                        i+=1
-                    else:
-                        list1[k]=right[j]
-                        j+=1
-                    k+=1
-                while i<len(left):
-                    list1[k]=left[i]
-                    i+=1
-                    k+=1
-                while j<len(right):
-                    list1[k]=right[j]
-                    j+=1
-                    k+=1
-        merge_sort(nums)
-        return max(nums[-1]*nums[-2]*nums[-3],nums[0]*nums[1]*nums[-1])
+        max1=max2=max3=float("-inf")
+        min1=min2=float("inf")
+        for  num in nums:
+            if num>max1:
+                max3=max2
+                max2=max1
+                max1=num
+            elif num>max2:
+                max3=max2
+                max2=num
+            elif num>max3:
+                max3=num
+            if num<min1:
+                min2=min1
+                min1=num
+            elif num<min2:
+                min2=num
+        return max(max1*max2*max3, min1*min2*max1)
+            
