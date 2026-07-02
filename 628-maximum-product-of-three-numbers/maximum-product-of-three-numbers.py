8a597a6892b62896dae2e1279392c29bd9be012f
swayamkaserniya1225-source <swayamkaserniya1225@gmail.com>
2026-07-02 15:01:41 +0530

    Time: 1 ms (98.18%) | Memory: 20.5 MB (16.69%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.py
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.py
@@ -17,5 +17,5 @@ class Solution:
                 min1=num
             elif num<min2:
                 min2=num
-        return max(max1*max2*max3, min1*min2*max1)
+        return max(max1*max2*max3,max1*min1*min2)
             
