201f75f65a85a20a7f9e03e9e18ba245762ce60f
swayamkaserniya1225-source <swayamkaserniya1225@gmail.com>
2026-07-03 12:23:20 +0530

    Time: 9 ms (37.20%) | Memory: 21.4 MB (19.60%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -3,13 +3,12 @@ class Solution:
         element=None
         count=0
         for num in nums:
+            if count==0:
+                element=num
             if element==num:
                 count+=1
             else:
                 if count>0:
                     count-=1
-            if count==0:
-                element=num
-                count=1
         return element            
         
