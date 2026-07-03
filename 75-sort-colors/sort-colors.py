7841bf8ffe74f28787fdb8c9ce02a0a2b97001a5
swayamkaserniya1225-source <swayamkaserniya1225@gmail.com>
2026-07-03 13:37:58 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (21.49%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,30 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        def merge_sort(list1):
-            if len(list1)>1:
-                mid=len(list1)//2
-                left=list1[:mid]
-                right=list1[mid:]
-                merge_sort(left)
-                merge_sort(right)
-                i=j=k=0
-                while i<len(left) and j<len(right):
-                    if left[i]<right[j]:
-                        list1[k]=left[i]
-                        i+=1
-                    else:
-                        list1[k]=right[j]
-                        j+=1
-                    k+=1
-                while i<len(left):
-                    list1[k]=left[i]
-                    i+=1
-                    k+=1
-                while j<len(right):
-                    list1[k]=right[j]
-                    j+=1
-                    k+=1
-        merge_sort(nums)
+        low,mid,high=0,0,len(nums)-1
+        while mid<=high:
+            if nums[mid]==0:
+                nums[low],nums[mid]=nums[mid],nums[low]
+                low+=1
+                mid+=1
+            elif nums[mid]==1:
+                mid+=1
+            else:
+                nums[high],nums[mid]=nums[mid],nums[high]
+                high-=1
 
-        
