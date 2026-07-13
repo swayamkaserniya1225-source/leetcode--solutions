046c79ee52b0c13e00f77d2aacefddf541f57c56
swayamkaserniya1225-source <swayamkaserniya1225@gmail.com>
2026-07-13 15:38:07 +0530

    Time: 4 ms (69.09%) | Memory: 22.8 MB (80.36%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -2,8 +2,6 @@ import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         hashtable=Counter(nums)
-        for num in nums:
-            hashtable[num]=hashtable.get(num,0)+1
         heap=[]
         for  key,value in hashtable.items():
             heapq.heappush(heap,(value,key))
