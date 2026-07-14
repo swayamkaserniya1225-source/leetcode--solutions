1a7d9345f67307e6bebd1890c9df431bab15ea0c
swayamkaserniya1225-source <swayamkaserniya1225@gmail.com>
2026-07-14 11:45:35 +0530

    Time: 15 ms (44.37%) | Memory: 19.5 MB (29.39%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        hashtable=[0]*26
+        for i in range(len(s)):
+            index=ord(s[i])-ord("a")
+            hashtable[index]+=1
+            index1=ord((t[i]))-ord("a")
+            hashtable[index1]-=1
+        return len(set(hashtable))==1
