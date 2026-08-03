277d1c2c78d87c358ce2ba4af81f87096e853222
swayamkaserniya1225-source <swayamkaserniya1225@gmail.com>
2026-08-03 15:47:15 +0530

    Time: 11 ms (77.12%) | Memory: 19.4 MB (75.57%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -6,7 +6,4 @@ class Solution:
         for i in range(len(s)):
             dict1[s[i]]=dict1.get(s[i],0)+1
             dict1[t[i]]=dict1.get(t[i],0)-1
-        for key,values in dict1.items():
-            if values!=0:
-                return False
-        return True
+        return all(values==0 for values in dict1.values())
