60e51d37504576f984a36e5408a1db84f56c2d5e
swayamkaserniya1225-source <swayamkaserniya1225@gmail.com>
2026-08-04 11:28:25 +0530

    Time: 3 ms (73.55%) | Memory: 23.3 MB (75.07%) - LeetSync

--- 890-lemonade-change/lemonade-change.py
+++ 890-lemonade-change/lemonade-change.py
@@ -1,24 +1,24 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
         collection=0
-        count5=0
-        count10=0
-        count20=0
+        five=0
+        ten=0
         for bill in bills:
             if bill==5:
-                count5+=1
+                five+=1
             elif bill==10:
-                count10+=1
-                count5-=1
-            elif bill==20:
-                if count10>0:
-                    count10-=1
-                    count5-=1
+                if five<=0:
+                    return False
+                five-=1
+                ten+=1
+            else:
+                if ten>0 and five>0:
+                    ten-=1
+                    five-=1
+                elif ten==0 and five>=3:
+                    five-=3
                 else:
-                    count5-=3
-                count20+=1
-            if count5<0 or count10<0:
-                return False
+                    return False
         return True
 
             
