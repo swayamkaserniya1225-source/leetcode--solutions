b472e69acc397e4d7cebdc8f07e20d090135021e
swayamkaserniya1225-source <swayamkaserniya1225@gmail.com>
2026-08-04 16:51:42 +0530

    Time: 3 ms (83.78%) | Memory: 19.1 MB (89.71%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -1,26 +1,18 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
         arr1=[]
-        left=0
+        start=0
+        end=0
         dict1={}
-        count=0
-        for right in range(len(s)):
-            if s[right] not  in  dict1:
-                dict1[s[right]]=[right,right]
-            else:
-                dict1[s[right]][1]=right
-        x=list(dict1.values())
-        max_val=x[0][1]
-        start=[0][0]
-        for i in range(1,len(x)):
-            if max_val<x[i][0]:
-                arr1.append(max_val-start+1)
-                start=x[i][0]
-                max_val=x[i][1]
-            else:
-                max_val=max(max_val,x[i][1])
-        arr1.append(max_val-start+1)
+        for i,ch  in enumerate(s):
+            dict1[ch]=i
+        for i,ch in  enumerate(s):
+            end=max(end,dict1[ch])
+            if i==end:
+                arr1.append(end-start+1)
+                start=i+1
         return arr1
+
 
 
 
