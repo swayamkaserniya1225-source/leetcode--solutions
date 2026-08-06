35ff9073562f958febc4534d2dc831a8b775874b
swayamkaserniya1225-source <swayamkaserniya1225@gmail.com>
2026-08-06 16:58:23 +0530

    Time: 1277 ms (50.60%) | Memory: 58 MB (69.67%) - LeetSync

--- 204-count-primes/count-primes.py
+++ 204-count-primes/count-primes.py
@@ -2,16 +2,11 @@ class Solution:
     def countPrimes(self, n: int) -> int:
         if n<=2:
             return 0
-        seive=[True]*(n+1)
+        seive=[True]*(n)
         seive[0]=False
         seive[1]=False
         for  i in range(2,int(n**(0.5)+1)):
-            j=2
             if seive[i]:
                 for j in range(i*i,n,i):
                     seive[j]=False
-        count=0
-        for i in range(n):
-            if seive[i]:
-                count+=1
-        return count
+        return sum(seive)
