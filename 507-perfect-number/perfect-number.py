387bd21e4569a1630557815affdff00dcf2a4602
swayamkaserniya1225-source <swayamkaserniya1225@gmail.com>
2026-08-08 14:19:07 +0530

    Time: 6 ms (45.34%) | Memory: 19.2 MB (85.68%) - LeetSync

---
+++ 507-perfect-number/perfect-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        if num<=1:
+            return False
+        count=1
+        for i in range(2,int(num**0.5)+1):
+            if num%i==0:
+                count+=i
+                if num//i!=i:
+                    count+=num/i
+        return count==num
+        
